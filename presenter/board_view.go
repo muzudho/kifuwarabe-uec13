72bfc52ba6e0f570f2713c9eb48bc5d427a54299
むずでょ <muzudho1@gmail.com>
2022-01-25 02:04:58 +0900

    Print

--- presenter/board_view.go
+++ presenter/board_view.go
@@ -1,11 +1,10 @@
 package presenter
 
 import (
-	"fmt"
-	"os"
 	"strconv"
 	"strings"
 
+	code "github.com/muzudho/kifuwarabe-uec13/coding_obj"
 	e "github.com/muzudho/kifuwarabe-uec13/entities"
 )
 
@@ -98,5 +97,5 @@ func PrintBoard(board e.IBoard, movesNum int) {
 	}
 	b.WriteString("\n")
 
-	fmt.Fprint(os.Stderr, b.String())
+	code.Console.Notice(b.String())
 }

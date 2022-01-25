f70894f86c47519d49d7c72d41523801efad5692
むずでょ <muzudho1@gmail.com>
2022-01-25 19:35:00 +0900

    Print Z 修正

--- presenter/z.go
+++ presenter/z.go
@@ -3,6 +3,7 @@ package presenter
 import (
 	"fmt"
 
+	code "github.com/muzudho/kifuwarabe-uec13/coding_obj"
 	e "github.com/muzudho/kifuwarabe-uec13/entities"
 )
 
@@ -13,13 +14,15 @@ func GetGtpZ(board e.IBoard, z int) string {
 	}
 
 	var y = z / board.SentinelWidth()
-	var x = z - y*board.SentinelWidth()
+	var x = z % board.SentinelWidth()
 
 	// 筋が25（'Z'）より大きくなることは想定していません
 	var alphabet_x = 'A' + x - 1
 	if alphabet_x >= 'I' {
 		alphabet_x++
 	}
+
+	code.Console.Debug("y=%d x=%d z=%d alphabet_x=%d alphabet_x(char)=%c\n", y, x, z, alphabet_x, alphabet_x)
 
 	return fmt.Sprintf("%c%d", alphabet_x, y)
 }
